5cb39772cadbe8a26c13c3055e126bc7e3c4e60a
Maksym_Shveda <maksim1shveda@gmail.com>
2021-12-20 05:08:47 +0200

    ability to filter users using checkboxes for user's sex, made app simple responsive, corrected styles; pre-final version

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
   const [users, setUsers] = useState([]);
   const [initialUsers, setInitial] = useState([]);
   const [searchValue, setSearch] = useState({name:'', lastname:'', age:''})
+  const [checkboxes, setCheckboxes] = useState([false, false])
   const [isLoaded, setLoaded] = useState(false);
 
   
@@ -22,25 +23,42 @@ function App() {
       setLoaded(true);
     };
     requestForUsers();
-  },[])
+  },[]);
 
   useEffect(() => {
     setUsers(initialUsers);
     setUsers(previous => previous.filter((user) => {
       return user.name.toLowerCase().includes(searchValue.name.toLowerCase())
       && user.lastname.toLowerCase().includes(searchValue.lastname.toLowerCase())
-    }))
+    }));
     if(searchValue.age){
-      setUsers(previous => previous.filter((user) => user.age == searchValue.age ))
+      setUsers(previous => previous.filter((user) => user.age == searchValue.age ));
     }
-  }, [searchValue])
+    if(checkboxes[0] && checkboxes[1]){
+      return null;
+    }
+    else if(checkboxes[0]){
+      setUsers(previous => previous.filter((user) => user.sex === 'm'));
+    }
+    else if(checkboxes[1]){
+      setUsers(previous => previous.filter((user) => user.sex === 'f'));
+    }
+  }, [searchValue, checkboxes]);
+
 
   const handleSearch = (event) => {
     setSearch((previous) => ({
       ...previous,
       [event.target.name]: event.target.value
-    }))
+    }));
   };
+
+  const handleCheck = (position) => {
+    setCheckboxes((previous) => previous.map((item, index) => index === position ? !item : item ));
+  };
+
+  
+
 
   return (
     <div className="App">
@@ -52,6 +70,7 @@ function App() {
               <input
               type="text"
               name="name" 
+              value={searchValue.name}
               placeholder='Search By First Name...' 
               className='search-input'
               onInput={(e) => handleSearch(e)}
@@ -59,6 +78,7 @@ function App() {
               <input
               type="text" 
               name="lastname"
+              value={searchValue.lastname}
               placeholder='Search By Last Name...' 
               className='search-input'
               onInput={(e) => handleSearch(e)}
@@ -66,17 +86,28 @@ function App() {
               <input
               type="text"
               name="age"
+              value={searchValue.age}
               placeholder='Search By Age...' 
               className='search-input'
               onInput={(e) => handleSearch(e)}
               />
               <div className='sex checkboxes'>
                 <div>
-                  <input type="checkbox" name="male"/>
+                  <input 
+                  type="checkbox" 
+                  name="male"
+                  checked={checkboxes[0]}
+                  onChange={() => handleCheck(0)}
+                  />
                   <label htmlFor='male'>Male</label>
                 </div>
                 <div>
-                  <input type="checkbox" name="female"/>
+                  <input 
+                  type="checkbox" 
+                  name="female"
+                  checked={checkboxes[1]}
+                  onChange={() => handleCheck(1)}
+                  />
                   <label htmlFor='female'>Female</label>
                 </div>          
               </div>
