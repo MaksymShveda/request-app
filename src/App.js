7ba15450fcfe3deed3bb90d9a77bb64d347bfe7e
Maksym_Shveda <maksim1shveda@gmail.com>
2021-12-20 03:42:56 +0200

    added fetch, displaying users array to page; also added loader while data isnt ready

--- src/App.js
+++ src/App.js
@@ -2,31 +2,53 @@ import User from './components/User/User';
 import Header from './components/Header/Header';
 
 import { useState, useEffect } from 'react';
+import Loading from './components/Loading/Loading';
 
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [isLoaded, setLoaded] = useState(false)
+
+  useEffect(()=>{
+    const requestForUsers = async () => {
+      const response = await fetch('https://venbest-test.herokuapp.com/');
+      const usersData = await response.json();
+      setUsers((previous) => usersData);
+      setLoaded(true);
+    }
+    requestForUsers();
+  },[])
+
+
   return (
     <div className="App">
       <Header/>
       <div className='main-content'>
-        <div className='filter-panel'>
-          <input type="text" placeholder='Search By First Name...' className='search-input'></input>
-          <input type="text" placeholder='Search By Last Name...' className='search-input'></input>
-          <input type="text" placeholder='Search By Age...' className='search-input'></input>
-          <div className='sex checkboxes'>
-            <div>
-              <input type="checkbox" name="male"/>
-              <label htmlFor='male'>Male</label>
+      {isLoaded ?
+          <>
+            <div className='filter-panel'>
+              <input type="text" placeholder='Search By First Name...' className='search-input'></input>
+              <input type="text" placeholder='Search By Last Name...' className='search-input'></input>
+              <input type="text" placeholder='Search By Age...' className='search-input'></input>
+              <div className='sex checkboxes'>
+                <div>
+                  <input type="checkbox" name="male"/>
+                  <label htmlFor='male'>Male</label>
+                </div>
+                <div>
+                  <input type="checkbox" name="female"/>
+                  <label htmlFor='female'>Female</label>
+                </div>          
+              </div>
             </div>
-            <div>
-              <input type="checkbox" name="female"/>
-              <label htmlFor='female'>Female</label>
-            </div>          
-          </div>
-        </div>
+            <div className='users-wrapper'>
+              {users.map((user) => <User key={user.name} user={user}/>)}
+            </div>
+            </>
+          :( <Loading/> )}
       </div>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,9 +1,9 @@
-const User = () => {
+const User = ({user}) => {
     return (
         <div className="user-container">
-            <h4>Name:</h4>
-            <p>Age:</p>
-            <p>Sex:</p>
+            <h4>Name: {`${user.name} ${user.lastname}`}</h4>
+            <p>Age: {user.age}</p>
+            <p>Sex: {user.sex}</p>
         </div>
     )
 };
